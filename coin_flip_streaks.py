074bff3df26d00640cd7ec05198bfe18bef38a52
Devon Kong <devonkong@icloud.com>
2020-05-26 00:00:47 +1000

    Used regex to find and record patterns in coin flips.

--- coin_flip_streaks.py
+++ coin_flip_streaks.py
@@ -1,16 +1,29 @@
-import random
-number_of_streaks = 0
+# A program to find out how often a streak of six heads or tails occurs.
 
-for experiment_number in range(10000):
-    # This creates a list of 100 'heads' or 'tails' values
-    coin_flip_results = []
+import random, re
+
+streak_counter = 0
+streak_heads = r'HHHHHH'
+streak_tails = r'TTTTTT'
+
+for experiment in range(1):
+    # Records occurences of heads or tails in a string.
+    observations = ''
     for coin_flip in range(100):
         if random.randint(0, 1) == 0:
-            coin_flip_results.append('H')
+            observations += 'H'
         else:
-            coin_flip_results.append('T')
+            observations += 'T'
+    
+    # Checks for streaks of 6 heads or tails and adds to the counter.
+    print(observations)
+    streak_counter += len(re.findall(streak_heads, observations))\
+                       + len(re.findall(streak_tails, observations))
+    print(f'The streak counter is now {streak_counter}.')
+    
 
-    # Code that checks if there is a streak of 6 heads or tails in a row
+### Todo:   Replace streak_counter with list and find mean.
+###         Estimate percentage mathematically to test accuracy of results.
 
 
-print(f'The chance of 6 heads or tails occuring in a row is {number_of_streaks/100}.')
+print(f'The chance of 6 heads or tails occuring in a row is {streak_counter/100}%.')
