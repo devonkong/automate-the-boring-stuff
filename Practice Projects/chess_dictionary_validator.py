ec59023e6509c862eb82495005ad5a20d78038d4
Devon Kong <devonkong@icloud.com>
2020-06-01 20:47:44 +1000

    Fixed blank space bug, code optimisation and testing.

--- Practice Projects/chess_dictionary_validator.py
+++ Practice Projects/chess_dictionary_validator.py
@@ -1,7 +1,6 @@
 # Chapter 5: Chess Dictionary Validator
 # Write a function that takes a dictionary argument and returns a Boolean value depending on if the board is valid.
 # This function should detect when a bug has resulted in an improper chess board.
- 
     
 # Creating a list of valid coordinates on a chess board
 valid_coordinates = []
@@ -27,47 +26,62 @@ def is_valid_chess_board(board):
     # Note: Each player must have exactly one king, at most 16 pieces and at most 8 pawns
     piece_counter = {'b-king': 0, 'b-pieces': 0, 'b-pawns': 0,
                             'w-king': 0, 'w-pieces': 0, 'w-pawns': 0}
-    
-    def has_valid_chess_pieces(piece):
-        """A function that examines the argument piece and adds to the piece_counter dictionary, then checks if the number of pieces is valid."""
+        
+    # Check the validity of every coordinate in the dictionary
+    for coordinate, chess_piece in board.items():
+        if coordinate not in valid_coordinates:
+            print(f'{chess_piece.capitalize()} is on {coordinate}, which is not a valid coordinate.')
+            return False
         
         # Check which player the piece belongs to
-        if piece.startswith('b-'):
+        if chess_piece.startswith('b-'):
             player = 'b'
-        elif piece.startswith('w-'):
+        elif chess_piece.startswith('w-'):
             player = 'w'
         else:
-            print(f'{coordinate} was empty.')
-            return # stop running the function if the coordinate is empty
+            continue # skips the rest of the function if the coordinate is empty
         
         # Add to the piece_counter dictionary depending on the player and type of chess piece
         # Checks if each player has exactly one king, at most 16 pieces and at most 8 pawns
-        if piece.startswith(player):
+        if chess_piece.startswith(player):
             piece_counter[player + '-pieces'] += 1
-            if piece_counter[player + '-pieces'] > 16:
-                print(f'{player.capitalize()} has more than 16 pieces.')
-                return False
             if chess_piece == player + '-king':
-                piece_counter[piece] += 1
-                if piece_counter[piece] != 1:
-                    print(f'{player.capitalize()} does not have a king.')
+                piece_counter[player + '-king'] += 1
+                if piece_counter[player + '-king'] != 1:
+                    print(f'{player.capitalize()} does not have exactly one king.')
                     return False
             elif chess_piece == player + '-pawn':
                 piece_counter[player + '-pawns'] += 1
                 if piece_counter[player + '-pawns'] > 8:
                     print(f'{player.capitalize()} has more than 8 pawns.')
                     return False
-                
-    # Check the validity of every coordinate in the dictionary
-    for coordinate, chess_piece in board.items():
-        if coordinate not in valid_coordinates:
-            return False
+            elif piece_counter[player + '-pieces'] > 16:
+                print(f'{player.capitalize()} has more than 16 pieces.')
+                return False
         
-        # Run a function to record the chess piece found on each coordinate
-        if has_valid_chess_pieces(chess_piece) == False:
-            return False
-        
-    print(piece_counter)
+    print('The board is valid.')    
     return True  # Returns True if none of the invalid if conditions were triggered
         
+
+# Testing the function with a valid board, with all pieces on their starting positions.
 print(is_valid_chess_board(chess_board))
+
+# Testing a board where B has two kings.
+chess_board_extra_king = chess_board.copy()
+chess_board_extra_king['8d'] = 'b-king'
+print(is_valid_chess_board(chess_board_extra_king))
+
+# Testing a board where W more than 16 pieces.
+chess_board_extra_pieces = chess_board.copy()
+chess_board_extra_pieces['6f'] = 'w-queen'
+print(is_valid_chess_board(chess_board_extra_pieces))
+
+# Testing a board where B has more than 8 pawns.
+chess_board_extra_pawn = chess_board.copy()
+chess_board_extra_pawn['6f'] = 'b-pawn'
+print(is_valid_chess_board(chess_board_extra_pawn))
+
+# Testing a board where W has a piece on an invalid coordinate.
+chess_board_invalid_coord = chess_board.copy()
+chess_board_invalid_coord['9a'] = 'w-rook'
+print(is_valid_chess_board(chess_board_invalid_coord))
