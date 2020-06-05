be895ac2a0901c6dd5bd65c971aa7bb54d9999e5
Devon Kong <devonkong@icloud.com>
2020-06-05 11:35:33 +1000

    Added ability to check large strings.

--- Tutorial Files/is_phone_number.py
+++ Tutorial Files/is_phone_number.py
@@ -26,3 +26,15 @@ print(is_phone_number(test_input_1))
 
 print(f"Is {test_input_2} a phone number?")
 print(is_phone_number(test_input_2))
+
+
+# Checking for a phone number within a larger string
+
+message = "Hi, I think you're cute. I'd like to meet you for coffee sometime. My phone number is 444-444-6969. Or you can call me at 414-414-4040 for... privacy reasons."
+
+for i in range(len(message)):
+    chunk = message[i:i + 12]
+    if is_phone_number(chunk):
+        print(f"Phone number found: {chunk}")
+        
+print("Done.")
