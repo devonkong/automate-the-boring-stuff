fc309c9d0e79fd497e2d5e77f012051c3daf67fd
Devon Kong <devonkong@icloud.com>
2020-06-08 14:36:13 +1000

    Added missing escape character to regex object.

--- Chapter Projects/phone_and_email.py
+++ Chapter Projects/phone_and_email.py
@@ -5,9 +5,9 @@ import pyperclip, re
 
 # Create phone regex
 phone_regex = re.compile(r"""(  # 0450 555 555 / 0450555555 / 61450555555
-+?(61)?                         # Country code
+(61|\+61)?                        # Country code
 \s?                             # Separator
-(0?\d{3})                       # First four digits
+(0?4\d{2})                        # First four digits
 \s?                             # Separator
 (\d{3})                         # Three digits
 \s?                             # Separator
@@ -19,7 +19,7 @@ email_regex = re.compile(r"""(
 [a-zA-Z0-9.-_+%]+               # Username
 @                               # @ symbol
 [a-zA-Z0-9.-]+                  # Domain name
-(\.[a-zA-Z])+                   # TLD
+(\.[a-zA-Z]+)                   # TLD
 )""", re.VERBOSE)
 
 # Find matches in clipboard text
@@ -27,10 +27,13 @@ text = str(pyperclip.paste())
 
 matches = []
 for groups in phone_regex.findall(text):
-    phone_num = '-'.join([groups[2], groups[3], groups[4]])
+    phone_num = ' '.join([groups[2], groups[3], groups[4]])
     if groups[1] != '':
         phone_num = '0' + phone_num
     matches.append(phone_num)
+    
+# Add a newline between phone numbers and emails    
+matches.append('')
     
 for groups in email_regex.findall(text):
     matches.append(groups[0])
