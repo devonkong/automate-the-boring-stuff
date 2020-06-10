1f4235e4b90cba429c7b1e380c4163354a3701f2
Devon Kong <devonkong@icloud.com>
2020-06-10 23:47:30 +1000

    Now checks for and validates leap years. Replaced some for loops with list comprehensions.

--- Practice Projects/date_detection.py
+++ Practice Projects/date_detection.py
@@ -15,13 +15,12 @@ date_regex = re.compile(r"""(
 
 
 # Test regex
-test_string = "Testing regex with 01/01/1000, 31/02/2020, 31/04/2021 and 31/12/2999, which are technically valid dates, and some impossible dates: 00/01/2020, 01/00/2020, 32/01/2020 and 01/13/2020."
+test_string = "Testing regex with 01/01/1000, 31/02/2020, 29/02/2020, 31/04/2021 and 31/12/2999, which are technically valid dates, and some impossible dates: 00/01/2020, 01/00/2020, 32/01/2020 and 01/13/2020."
 print(test_string, end = '\n\n')
 
 extracted_dates = date_regex.findall(test_string)
 print('Dates found:')
-for match in extracted_dates:
-    print(match[0])
+[print(match[0]) for match in extracted_dates]
 
 
 # Function to check if date is valid
@@ -38,21 +37,23 @@ def date_validator(dates):
             else:
                 print(f"{match[0]} is not a valid date.")
 
-        # Validate February on non-leap years, which has 28 days
+        # Validate February, which has 28 days during non-leap years and 29 days during leap years
         elif match[2] == "02":
-            if int(match[1]) <= 28:
+            # Check if days are 29 or less during leap year
+            if int(match[3]) % 4 == 0 and (int(match[3]) % 100 != 0 or int(match[3]) % 400 == 0) and int(match[1]) <= 29:
+                valid_dates.append(match[0])
+            # Check if days are 28 or less during non-leap year
+            elif int(match[1]) <= 28:
                 valid_dates.append(match[0])
             else:
                 print(f"{match[0]} is not a valid date.")
-
-        # TODO: Check for leap year
-
         else:
             valid_dates.append(match[0])
 
-    print(valid_dates)
+    # Print out valid dates
+    print("\nValid dates:")
+    [print(date) for date in valid_dates]
 
+    
+# Run date validator function
 date_validator(extracted_dates)
-
-
-# TODO: Match and validate from clipboard
